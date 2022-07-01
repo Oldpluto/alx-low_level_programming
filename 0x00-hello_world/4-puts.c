7f96e9b0fbde3364ea03e3d89c34d870d9e5729a
Pluto <info.oldplutox@gmail.com>
2022-06-30 21:07:23 -0700

    first c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *   main - prints the phrase "Programming is like building a multilingual puzzle
  *   * Return: 0 if executed successfully, non-zero otherwise
- *    */
+     */
 
 int main(void)
 {
