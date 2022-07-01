0dba260cb2cd0a3ecb066d6e8067ed4d151729b1
Pluto <info.oldplutox@gmail.com>
2022-06-30 21:18:52 -0700

    writ a C program with Printf'

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/** main-prints the phrase
+ * "with proper grammar, but the outcome is a piece of art,"
+ * return: 0 if executed successfully ,nonzero otherwise
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
