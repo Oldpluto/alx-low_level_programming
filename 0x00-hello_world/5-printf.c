30278efdb90a9f8ee3fe87f957cffd025c962463
Pluto <info.oldplutox@gmail.com>
2022-06-30 21:27:47 -0700

    program of C with printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
-/** main-prints the phrase
- * "with proper grammar, but the outcome is a piece of art,"
+/** 
+ * main-prints the phrase
  * return: 0 if executed successfully ,nonzero otherwise
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
