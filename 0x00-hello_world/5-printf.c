3183bbb578b50e1c47cf9743e637c4cd89297def
Pluto <info.oldplutox@gmail.com>
2022-06-30 21:37:44 -0700

    second c program after this

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-
 /**
- * main-prints the phrase
- * return: 0 if executed successfully ,nonzero otherwise
+ * main - prints the phrase
+ * return: 0
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
