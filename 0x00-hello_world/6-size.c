b3de986c2124a9feb5646f538f18a7a1b9205568
Pluto <info.oldplutox@gmail.com>
2022-07-03 14:49:58 -0700

    printing file of various sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,7 +15,7 @@ int main(void)
 	printf("Size of a char: %d byte(s)\n", sizeof(c));
 	printf("Size of a int: %d bytes(s)\n", sizeof(i));
 	printf("Size of long int: %d byte(s)\n", sizeof(li));
-	printf("Size of long log int: %d byte(s)\n", sizeof(lli));
+	printf("Size of long long int: %d byte(s)\n", sizeof(lli));
 	printf("Size of float: %d byte(s)\n", sizeof(f));
 	return (0);
 }
