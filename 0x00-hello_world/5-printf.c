bbca0da7df239c0c5a8641f2f6cdbc5d409f688f
Pluto <info.oldplutox@gmail.com>
2022-07-03 14:14:43 -0700

    okay done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
-/* main - Where to enter the print content*/
-
-int main(void)
-/* main - print (success) 
-  */
-	
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
