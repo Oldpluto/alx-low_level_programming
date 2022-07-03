293f455ded437d240cf3b1bf7a20fb1b6b3e738d
Pluto <info.oldplutox@gmail.com>
2022-07-03 15:39:15 -0700

    print with the fuction write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printing something lol
+ * return: 1 if runn successfull
+ **/
+
+int main(void)
+{
+	write("and that piece of art is useful"" - Dora Korpar, 2015-10-19\n");
+	return (1);
+
+}
