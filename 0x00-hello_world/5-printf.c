07456dca8cfa36c7107bcd92b0acec4f6fd3cda4
Pluto <info.oldplutox@gmail.com>
2022-07-03 14:07:01 -0700

    betty doc style

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,6 +2,9 @@
 /* main - Where to enter the print content*/
 
 int main(void)
+/* main - print (success) 
+  */
+	
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
