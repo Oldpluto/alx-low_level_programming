5e76d6807359139fbaccd8f13c2c4c23bcb10be7
Pluto <info.oldplutox@gmail.com>
2022-07-15 00:11:53 -0700

    print number

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ void reverse_array(int *a, int n);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+void print_number(int n);
 char *string_toupper(char *);
 int _putchar(char c);
 char *_strncpy(char *dest, char *src, int n);
