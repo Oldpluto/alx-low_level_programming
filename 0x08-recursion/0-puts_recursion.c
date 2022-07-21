896921745b498633e9523b1b5c8e6ddaa7466e7b
Pluto <info.oldplutox@gmail.com>
2022-07-21 06:13:35 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+/*
+ * main - check out code
+ *
+ * Return : 0.
+ */
+
+void _puts_recursion(char *s)
+{
+	if(*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(*s);
+	s++;
+	_puts_recursion(s)
+}
