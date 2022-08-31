4f057d46bdba916b25b28fef16ac42a86bee3e10
Pluto <info.oldplutox@gmail.com>
2022-08-30 20:48:09 -0700

    guess who's coding in c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /*
  * main - printing the statement 
- * return - 0 
+ * return - 0
  */
-
 int main(void)
 {
+	/* main function
+	 */
+
 	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 
 	return (1);
