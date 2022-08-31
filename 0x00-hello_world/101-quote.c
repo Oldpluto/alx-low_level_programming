d14fa73c674574d73640300b75094a669493a1ce
Pluto <info.oldplutox@gmail.com>
2022-08-30 21:01:03 -0700

    coding c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	/* main function */
 
-	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 2);
 
 	return (1);
 }
