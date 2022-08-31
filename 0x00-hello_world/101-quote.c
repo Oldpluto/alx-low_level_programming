1c55e0355083836464046657d71adb4c6a24f030
Pluto <info.oldplutox@gmail.com>
2022-08-30 20:34:17 -0700

    I am Back

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
-#include <unistd.h>
-/**
- * main - printing something lol
- * return: 1 if runn successfull
- **/
+#include <stdlib.h>
 
-int main(void)
+int main()
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-	return (1);
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 
+	return 1;
 }
