7b1b31b0c8d79cea33d5630f32c9ed0365d503ce
Pluto <info.oldplutox@gmail.com>
2022-08-31 19:02:25 -0700

    lowercase alphabet'

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - printing alphabets in lowercase
+ *
+ * return: 0 (success)
+ */
+
+int main(void)
+{
+	putchar(" ((a-z))\n");
+
+	return (0);
+}
