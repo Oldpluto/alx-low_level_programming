4cb3109f4aa6c6e5fd7fa28f9dd6fb61694e17f9
Pluto <pascaliroro6@gmail.com>
2022-09-05 08:43:43 -0700

    single numbers'

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints all Single numbers from 0 - 9
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int single_num;
+
+	for (single_num = '0'; single_num <= '9'; single_num++)
+	{
+		printf(single_num);
+	}
+
+	printf('\n');
+	return (0);
+}
