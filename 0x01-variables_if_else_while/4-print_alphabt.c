6a603f1fdb7480bc3ece8f433c72406fec9a6222
Pluto <pascaliroro6@gmail.com>
2022-09-05 07:58:01 -0700

    Print alphabets without q & e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - Prints all alphabets excepts q and e in lowercase
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <='z'; a++)
+	{
+		if (a != 'e' && a != 'q'; a++)
+		{
+			putchar('a');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
