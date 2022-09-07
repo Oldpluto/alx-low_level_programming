5e5a2b3cca6ec2df8544004646e45afaee6f0986
Pluto <pascaliroro6@gmail.com>
2022-09-07 08:10:29 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print HexDecimal Numbers
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
