fba365a451734ea706b51103f8dbc2bf0e3a1a75
Pluto <pascaliroro6@gmail.com>
2022-09-07 08:02:28 -0700

    Print combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print all possible single digit
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar(", ");
+
+	return (0);
+}
