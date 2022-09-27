6ea8bb6aaa425a8d59e830af12bc25f64ab921a8
Pluto <pascaliroro6@gmail.com>
2022-09-26 19:22:06 -0700

    Combination of three digits'

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Print combination of 3 digits 
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = '0'; b <= '9'; b++)
+		{
+			for (c = '0'; c <= '9'; c++)
+			{
+				if (a < b < c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+
+					if (a != '8' || (a =='8' && c != '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
