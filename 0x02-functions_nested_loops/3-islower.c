e3b20d65fae1d03f310cc15f590207c840807cf9
Pluto <pascaliroro6@gmail.com>
2022-10-03 20:24:24 -0700

    Lower case find

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _islower - Print 0 or 1
+ *
+ * Return: 0 or 1
+ */
+
+int _islower(int c)
+{
+	int a;
+
+	if (a = 'a'; a <= 'z')
+	{
+		return 1;
+	}
+	else
+		return 0;
+}
