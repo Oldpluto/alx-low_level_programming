0e652c412a982f38bb11f7a109b78c1fcafeeb77
Pluto <pascaliroro6@gmail.com>
2022-10-03 18:58:18 -0700

    Print alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int a;
 
-	for print_alphabet(a = 'a'; a <= 'z'; a++)
+	for (a = 'a'; a <= 'z'; a++);
 	{
 		_putchar print_alphabet(a);
 	}
 
 	_putchar ('\n');
 	
-	return 0;
+	return(0);
