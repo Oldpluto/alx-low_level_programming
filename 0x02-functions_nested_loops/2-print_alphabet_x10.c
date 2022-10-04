637ed13e9379a59b7e13c062bf96bea886c625ff
Pluto <pascaliroro6@gmail.com>
2022-10-03 19:27:50 -0700

    print alphabets 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet 10 times
+ *
+ * return: void
+ */
+
+void print_alphabet_x10(void);
+{
+	int a;
+
+	if (a<11)
+
+	for (a = 'a'; a <= 'z'; a++);
+	{
+		_putchar(a);
+	}
+
+	_putchar('\n');
+}
