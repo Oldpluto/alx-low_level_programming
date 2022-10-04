b550ace10e3298fdd01c971e5986f7eb997d9d64
Pluto <pascaliroro6@gmail.com>
2022-10-03 18:56:16 -0700

    Printing alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Printing the alphabets
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int a;
+
+	for print_alphabet(a = 'a'; a <= 'z'; a++)
+	{
+		_putchar print_alphabet(a);
+	}
+
+	_putchar ('\n');
+	
+	return 0;
