3822b3cbc2bbcedc1c24d84bc9683709d1d930ff
Pluto <pascaliroro6@gmail.com>
2022-10-04 19:44:13 -0700

    Print signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_sign - printing either + - or 0
+ * @n: character 
+ *
+ * Return: 0 (success)
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+
+		return (1);
+	}
+	
+	else if (n = 0)
+	{
+		_putchar('0');
+
+		return (0);
+	}
+
+	else 
+	{
+		_putchar('-');
+
+		return (-1);
+	}
+}
+
