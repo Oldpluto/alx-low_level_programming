68cb3e204a9dd98ce8bf57baf2e0bf8f71c7a3a7
Pluto <pascaliroro6@gmail.com>
2022-10-04 20:35:55 -0700

    Print absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - Print the absolute value of an integer
+ *
+ * Return: 0 (success)
+ */
+
+int _abs(int)
+{
+	int a;
+
+	a = abs(a);
+	{
+		_putchar(a);
+	}
+	return (0);
+}
