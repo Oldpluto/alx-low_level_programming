ce02447cf227fd9db5dc720438f5b5e142ae1545
Pluto <pascaliroro6@gmail.com>
2022-10-05 17:13:47 -0700

    Print last number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Printing last digit of a number
+ * @n: character as betty need it lol
+ *
+ * Return: 0 (success)
+ */
+
+int print_last_digit(int)
+{
+	int a, last;
+
+	a = last * 10;
+
+	{
+		_putchar("%d", a);
+	}
+
+	return (0);
+}
