3135215f1b4e746c5247070bffdc690bf54ed027
Pluto <pascaliroro6@gmail.com>
2022-10-06 04:27:45 -0700

    Printing mins of a man's day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Print the mins of jack bauer day
+ * @a: The minutes counter
+ *
+ * Return: No return
+ */
+
+void jack_bauer(void)
+{
+	int a;
+
+	while a = '00:00';
+
+	for (a >= '00:00'; a <= '23:59'; a++)
+	{
+		_putchar(a);
+		_putchar('\n');
+	}
+
+	a++;
+}
