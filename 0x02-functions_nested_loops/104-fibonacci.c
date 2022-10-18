75b93db5ca7b2db57e5b301ce6bc12a195c5f31d
Pluto <pascaliroro6@gmail.com>
2022-10-18 16:28:21 -0700

    first 98 fib number

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints th first 98 fib numbers
+ *
+ * Return: sucess
+ */
+
+int main(void)
+{
+	int a = 0;
+	long j =1, k = 2;
+
+	while (a < 98)
+	{
+		if (a == 0)
+			printf("%ld", j);
+		else if (a == 1)
+			printf(", %ld", k);
+		else
+		{
+			k += j;
+			j = k - j;
+			printf(", %ld", k);
+		}
+		a++;
+	}
+
+	printf("\n");
+	return (0);
+}
