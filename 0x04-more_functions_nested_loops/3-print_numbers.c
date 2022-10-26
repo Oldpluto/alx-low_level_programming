3841fe864ebf64990bad67338c128ee6e22f2801
Pluto <pascaliroro6@gmail.com>
2022-10-25 18:43:24 -0700

    Print 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - Print 0 - 9
+ *
+ * return: success
+ */
+
+void print_numbers(void)
+{
+	int c;
+
+	while (c => 48 && c <= 57, c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+}
